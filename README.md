4951c088950ff70905c24c2b79f269546640ece8
Daniel Tinoco <dbtinoco@dons.usfca.edu>
2023-06-30 17:20:44 -0700

    added data and slide materials

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/EbyQwFbY)
+# MSDS 631 Final Project
+
+Adam Ansari, Daniel Tinoco, Yusong Wang
+
+## Running the Notebook
+The following notebook details the process of creating a Huggingface style dataset of audio samples with their respective text and creating speaker embeddings using Speechbrain's VoxCeleb model. This is then passed into Microsoft's SpeechT5 model for generating speech given some text input.
+
+We recommend running this notebook on Google Colab with NVIDIA A100. During testing, we used approximately 200 computation units for 158 audio samples and 400 epochs. It should be mentioned that we ran this multiple times, so your units may vary. 10 to 20 units should suffice for a single run of the notebook with a similar data size.
+
+See the first cell of the notebook for the colab version of the notebook.
+
+### Downloading the files from Google Drive
+
+We used Google Drive with Colab to load in all our data. You can access the Drive at the below link. You will need to change the Pathing to your Drive paths. If you do not wish to use Colab or Drive you can instead download the data available in this repo. In addition at each of the core sections we pickled the final output and loaded in these pickles at the start of the next step. If you wish to start from any of the major sections you can instead load in the pickle files. Each major sections starts with code to load in these files and assumes you are doing this. As with all the paths in the notebooks they need to be changed to match yours. 
+
+https://drive.google.com/drive/folders/1T-hKJVBv9Ww4RPknT5WCwlVWbGKVHfwn?usp=sharing
+
+## Viewing the PowerPoint
+
+To view the slides, click here: https://docs.google.com/presentation/d/1W0wwmMcyRBXhGJQQ8YxpH1YOAs_UpkShZbJ1xXalUkg/edit?usp=sharing. A PDF version of the slides is provided in the repo but the PDF does not have sound file output at the end. For the best experience we reccomend going through the slides at the link above. 
---
+++ text/sam_text.py
@@ -0,0 +1,80 @@
+sam2text = {
+'_and-i-will-strike-down-upon-thee-with-great-vengeance-and-furious-anger-those-who-attempt-to-poison-and-destroy-my-brothers!_.wav':
+    f'And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers!',
+'_and-you-will-know-my-name-is-the-lord-when-i-lay-my-vengeance-upon-thee!_.wav':
+    'My brothers, and you will know my name is the lord when I lay my vengeance upon thee!',
+'ain_t-the-same-league-it-ain_t-the-same-fuckin_-sport.wav':
+    f'''The same ball fucking ball park it ain't the league it ain't even the same fucking sport''',
+'all_u_had_2_say.wav':
+    f'''Shit negro that's all you had to say.''',
+'for-your-own-good-stay-out-of-this-affair-i-sense-a-great-deal-of-confusion-in-you-young-skywalker-there-is-much-fear-that-clouds-your-judgment.wav':
+    f'''For your own good stay out of this affair. I sense a great deal of confusion in you young Sky walker. There is much fear that clouds your judgement.''',
+'goddamn-girl-how-you-live-like-this-like-what-like-this-this-some-repugnant-shit.wav':
+    f'''Goddam girl how you live like this? Like this. This some repugnant shit.''',
+'goddamn-jimmie!-this-some-serious-gourmet-shit.wav':
+    f'''Mmm. Goddamm Jimmie! This some serious gourmet shit.''',
+'ha!-never-heard-that-one-before.wav':
+    f'''Ha! Never heard that one before.''',
+'he-has-control-of-the-senate-and-the-courts!-he_s-too-dangerous-to-be-left-alive!.wav':
+    f'''He has control of the senate and the courts. He's too dangerous to be kept alive!''',
+'i-am-going-to-end-this-once-and-for-all.wav':
+    f'''I am going to end this once and for all.''',
+'i-mean-one-more-jolt-of-this-death-ray-and-i_m-an-epitaph.wav':
+    f'''So now I'm in deep trouble. I mean one more jolt of this death ray and I'm an epitaph.''',
+'i-mean-you-tell-me-you_re-a-super-mega-ultra-lightning-babe-that_s-all-right-with-me-i_m-good-i_m-good.wav':
+    f'''You go anything like that. I mean you tell me you're a super mega ultra lightning babe, that's alright with me. That's alright with me. I'm good. I'm good.''',
+'i-recognize-the-council-has-made-a-decision-but-given-that-it_s-a-stupid-ass-decision-i-have-elected-to-ignore-it.wav':
+    f'''I recognize the council has made a decision but given that it's a stupid ass decision I have elected to ignore it.''',
+'i-said-girl-i-don_t-want-to-know-about-your-mild-mannered-alter-ego-or-anything-like-that.wav':
+    f'''Like that. I said girl I don't want to know about your mild mannered alter ego or anything like that. I mean you tell.''',
+'it-actually-says-that-in-the-booklet-that-comes-with-it-_the-most-popular-gun-in-american-crime-_-like-they_re-proud-of-that-shit.wav':
+    f'''Can you believe that shit? It actually says that in the booklet that comes with it. The most popular gun in American crime, like they're proud of that shit.''',
+'just-because-you-pour-syrup-on-shit-don_t-make-it-pancakes-you-know-what-i_m-saying.wav':
+    f'''Want me to do, lie to you? Just cause you pour syrup on shit don't make it pancakes, you know what I'm saying?''',
+'look-what-if-we-actually-did-what-our-wives-think-we_re-doing-just-to-shake-things-up.wav':
+    f'''Rather go bowling. Look, what if we actually did what our wives think we're doing just to shake things up?''',
+'look-you-wanna-play-blind-man-go-walk-with-the-shepherd-but-me-my-eyes-are-wide-fuckin_-open.wav':
+    f'''Look you wanna play blind man go walk with the sheperd but me my eyes are wide fucking open.''',
+'me-and-vincent-would_ve-been-satisfied-with-some-freeze-dried-taster_s-choice-right-and-he-springs-this-serious-gourmet-shit-on-us.wav':
+    f'''Serious gourment shit. Me and Vincent would've been satisfied with some freeze dried taster's choice right and he springs this serious gourment shit on us.''',
+'my-ass-may-be-dumb-but-i-ain_t-no-dumb-ass.wav':
+    f'''My ass may be dumb, but I ain't no dumb ass.''',
+'no-no-no-no-that-shit-wasn_t-luck-this-was-divine-intervention.wav':
+    f'''No no no no that shit wasn't luck this was divine intervention.''',
+'now-that-my-friend-is-a-clear-cut-case-of-him-or-me-and-you-best-believe-it-ain_t-gonna-be-me.wav':
+    f'''Now that my friend is a clear cut case of him or me and you best believe it ain't gonna be me.''',
+'oh-uh-uh-uh-here-we-go-ak-47-the-very-best-there-is-when-you-absolutely-positively-got-to-kill-every-motherfucker-in-the-room-accept-no-substitutes.wav':
+    f'''Oh uh uh uh here we go AK 47 the very best there is when you absolutely positively got to kill every motherfucker in the room accept no substitutes.''',
+'say-_what_-again!-i-dare-ya!-i-double-dare-you-motherfucker!-say-_what_-one-more-goddamn-time!.wav':
+    f'''Say what again! Say what again! I dare ya! I double dare you motherfucker! Say what one more goddamm time!''',
+'sir-please-put-down-the-spear.wav':
+    f'''Sir please put down the spear.''',
+'sounds-like-a-shit-job-funny-i-was-thinkin_-the-same-thing.wav':
+    f'''When he wants it clean funny I was thinking the same thing.''',
+'superladies-they_re-always-trying-to-tell-you-their-secret-identity-think-it_ll-strengthen-the-relationship-or-something.wav':
+    f'''Super ladies they're always trying to tell you their secret identity, think it'll strengthen the relationship or something.''',
+'the-public-is-in-danger!-ocw.wav':
+    f'''The public is in danger!''',
+'there_s-no-pain-would-prise-his-need-from-him-a-lot-of-guys-think-that-until-the-pain-starts.wav':
+    f'''A lot of guys think that until the pain starts.''',
+'uh-uh-uh-i-didn_t-hear-you-wash-your-hands.wav':
+    f'''Uh uh uh i didn't hear you wash your hands.''',
+'until-such-time-as-the-world-ends-we-will-act-as-though-it-intends-to-spin-on.wav':
+    f'''Until such time as the world ends we will act as though it intends to spin on.''',
+'we_ve-made-some-mistakes-along-the-way-some-very-recently.wav':
+    f'''We've made some mistakes along the way some very recent.''',
+'where-is-my-supersuit-i0u.wav':
+    f'''Super suit where is my super suit?''',
+'yeah-you-say-peace-i-kind-of-think-you-mean-the-other-thing.wav':
+    f'''Yeah you say peace. I kind of think you mean the other thing.''',
+'you-done-slid-down-a-razor-blade-and-landed-in-a-alcohol-river.wav':
+    f'''You done slid down a razor blade and landed in a alcohol river.''',
+'you-ever-been-in-a-war-councilman-in-a-firefight-did-you-feel-an-overabundance-of-control.wav':
+    f'''You ever been in a war councilman in a firefight did you ever feel an over abundance of control?''',
+'you-have-made-me-very-desperate-you-might-not-be-glad-that-you-did.wav':
+    f'''You have made me very desperate you might not be glad that you did.''',
+'you-know-man-i-hate-to-be-the-kind-of-nigger-that-do-a-nigger-a-favor-then-bam-hit-the-nigger-up-for-a-favor-in-return-but-i-gots-to-be-that-kind-of-nigger.wav':
+    f'''You know man I hate to be the kind of nigger that do a nigger a favor then bam hit the nigger up for a favor in return but I gots to be that kind of nigger.''',
+'you-tell-me-where-my-suit-is-woman!-we-are-talking-about-the-greater-good.wav':
+    f'''You tell me where my suit is woman! We are talking about the greater good.'''
+}
